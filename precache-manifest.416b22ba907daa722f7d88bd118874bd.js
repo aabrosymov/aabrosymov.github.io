595bca94df2c3706f827d1f884d75023e10d7dd2
aabrosymov <aabrosymov@gmail.com>
2020-04-12 01:27:23 +0300

    Initial commit.

---
+++ precache-manifest.416b22ba907daa722f7d88bd118874bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7024b18493bd8a5d28910a1f9a5254e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2efc23a85b8f84edf37c",
+    "url": "/static/css/main.d41172bc.chunk.css"
+  },
+  {
+    "revision": "235cb83891015f51111c",
+    "url": "/static/js/2.f698496a.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/static/js/2.f698496a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2efc23a85b8f84edf37c",
+    "url": "/static/js/main.f16841b1.chunk.js"
+  },
+  {
+    "revision": "d114dbe615e96c6299b5",
+    "url": "/static/js/runtime-main.e044f45a.js"
+  }
+]);
